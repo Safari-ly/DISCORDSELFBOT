6787f1467c656493930a1c7fd90338d6ab15007a
Safari-ly <birupakshya890@gmail.com>
2025-02-24 00:28:19 +0530

    Update selfbot.py

--- selfbot.py
+++ selfbot.py
@@ -1,10 +1,10 @@
 #HEY! Before running, please fill out the info below. This is REQUIRED for the bot to run properly.
 
 #Paste your token in the quotations (this is so the bot can actually run on your account)
-token = ""
+token = "MTI4NDQ5NTczNTI3NDA3ODIzMA.GAmaYg.PFwPieRvxwwmhGTxBSfvi949tcIvjoj-Z8J6_8"
 
 #Type your prefix in the quotations, this will be placed in front of the chat commands.
-prefix = ""
+prefix = "&"
 
 #Enjoy! :)
 
